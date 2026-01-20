5ca773dadec7a052446370e00a0e15211e021306
YewFence <88962785+YewFence@users.noreply.github.com>
2026-01-20 18:25:21 +0800

    style(build): 为 shell 变量添加双引号
    为 build-images-windows.sh 中的变量引用添加双引号，
    遵循 shell 脚本最佳实践，防止变量值包含空格或
    特殊字符时出现意外的单词分割和通配符扩展问题。

--- build-images-windows.sh
+++ build-images-windows.sh
@@ -16,7 +16,7 @@ if [[ "${PUSH_IMAGES}" == "true" ]]; then
         echo "Building and pushing CI images"
         DO_PUSH="true"
     elif [[ "${GITHUB_REF}" =~ ^refs/tags/v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]]; then
-        RELEASE_VERSION=$(echo $GITHUB_REF | cut -c11-)
+        RELEASE_VERSION=$(echo "${GITHUB_REF}" | cut -c11-)
         echo "Releasing version ${RELEASE_VERSION}..."
         DO_PUSH="true"
     else
@@ -41,26 +41,26 @@ docker build -f Dockerfile-nanoserver . \
 
 if [[ "${DO_PUSH}" == "true" ]]; then
     if [[ "${GITHUB_REF}" == "refs/heads/fork-main" ]]; then
-        docker push ${IMAGE_NAME}:ci-nanoserver-1809
-        docker push ${IMAGE_NAME}:ci-nanoserver-ltsc2022
+        docker push "${IMAGE_NAME}:ci-nanoserver-1809"
+        docker push "${IMAGE_NAME}:ci-nanoserver-ltsc2022"
     elif [[ -n "${RELEASE_VERSION}" ]]; then
-        PATCH_VERSION=$(echo $RELEASE_VERSION | cut -c2-)
-        MINOR_VERSION=$(echo $PATCH_VERSION | cut -d. -f-2)
+        PATCH_VERSION=$(echo "${RELEASE_VERSION}" | cut -c2-)
+        MINOR_VERSION=$(echo "${PATCH_VERSION}" | cut -d. -f-2)
 
         # nanoserver-1809
-        docker tag ${IMAGE_NAME}:ci-nanoserver-1809 ${IMAGE_NAME}:nanoserver-1809
-        docker tag ${IMAGE_NAME}:ci-nanoserver-1809 ${IMAGE_NAME}:${PATCH_VERSION}-nanoserver-1809
-        docker tag ${IMAGE_NAME}:ci-nanoserver-1809 ${IMAGE_NAME}:${MINOR_VERSION}-nanoserver-1809
-        docker push ${IMAGE_NAME}:nanoserver-1809
-        docker push ${IMAGE_NAME}:${PATCH_VERSION}-nanoserver-1809
-        docker push ${IMAGE_NAME}:${MINOR_VERSION}-nanoserver-1809
+        docker tag "${IMAGE_NAME}:ci-nanoserver-1809" "${IMAGE_NAME}:nanoserver-1809"
+        docker tag "${IMAGE_NAME}:ci-nanoserver-1809" "${IMAGE_NAME}:${PATCH_VERSION}-nanoserver-1809"
+        docker tag "${IMAGE_NAME}:ci-nanoserver-1809" "${IMAGE_NAME}:${MINOR_VERSION}-nanoserver-1809"
+        docker push "${IMAGE_NAME}:nanoserver-1809"
+        docker push "${IMAGE_NAME}:${PATCH_VERSION}-nanoserver-1809"
+        docker push "${IMAGE_NAME}:${MINOR_VERSION}-nanoserver-1809"
 
         # nanoserver-ltsc2022
-        docker tag ${IMAGE_NAME}:ci-nanoserver-ltsc2022 ${IMAGE_NAME}:nanoserver-ltsc2022
-        docker tag ${IMAGE_NAME}:ci-nanoserver-ltsc2022 ${IMAGE_NAME}:${PATCH_VERSION}-nanoserver-ltsc2022
-        docker tag ${IMAGE_NAME}:ci-nanoserver-ltsc2022 ${IMAGE_NAME}:${MINOR_VERSION}-nanoserver-ltsc2022
-        docker push ${IMAGE_NAME}:nanoserver-ltsc2022
-        docker push ${IMAGE_NAME}:${PATCH_VERSION}-nanoserver-ltsc2022
-        docker push ${IMAGE_NAME}:${MINOR_VERSION}-nanoserver-ltsc2022
+        docker tag "${IMAGE_NAME}:ci-nanoserver-ltsc2022" "${IMAGE_NAME}:nanoserver-ltsc2022"
+        docker tag "${IMAGE_NAME}:ci-nanoserver-ltsc2022" "${IMAGE_NAME}:${PATCH_VERSION}-nanoserver-ltsc2022"
+        docker tag "${IMAGE_NAME}:ci-nanoserver-ltsc2022" "${IMAGE_NAME}:${MINOR_VERSION}-nanoserver-ltsc2022"
+        docker push "${IMAGE_NAME}:nanoserver-ltsc2022"
+        docker push "${IMAGE_NAME}:${PATCH_VERSION}-nanoserver-ltsc2022"
+        docker push "${IMAGE_NAME}:${MINOR_VERSION}-nanoserver-ltsc2022"
     fi
 fi
